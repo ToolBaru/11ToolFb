fc26d3e850b9597b21c6717eaf3b18b89ae27677
ToolBaru <62407117+ToolBaru@users.noreply.github.com>
2020-03-20 12:52:59 +0700

    Add files via upload

---
+++ HackFb.py
@@ -0,0 +1,130 @@
+#python
+impor  os
+impor  sys
+impor waktu
+dari  waktu  tidur impor 
+os . sistem ( "hapus" )
+def  jalan ( s ):
+  untuk  c  dalam  s  +  ' \ n ' :
+      sys . stdout . tulis ( c )
+      sys . stdout . flush ()
+      waktu . tidur ( 1. / 12 )
+jalan ( " \ x1b [1; 92mHallo selamat datang" )
+jalan ( " \ x1b [1; 93mIni adalah skrip Kumpulan Darkfb" )
+
+#utama
+sub  =  str ( input ( " \ 033 [1; 93mapa Anda sudah menjadi admin saluran subrek? y / t:" ))
+jika  sub  ==  "y" :
+   cetak ( " \ 033 [1; 92mTerimakasih Gan" )
+   os . sistem ( "hapus" )
+elif  sub  ==  "t" :
+     print ( " \ 033 [1; 92mSilahkan Subrek dulu" )
+     os . sistem ( "xdg-buka https://m.youtube.com/channel/UCsX7NpPEK8a0T2MCJJQ4o7w " )
+lain :
+  cetak ( " \ 033 [1; 97mInput tidak ada \ 033 [1; 91m !!!" )
+  os . sistem ( "python multidarkfb.py" )
+#spanduk
+tidur ( 2 )
+cetak ( " \ 033 [1; 93m ╭━┳━╭━╭━╮╮" )
+                                               
+cetak ( "┃   \ 033 [1; 97m ┣▅╋▅┫ \ 033 [1; 93m┃    \ 033 [1; 96m MULTI \ 033 [1; 93m" )
+cetak ( "┃ ┃ ╰━╰━━━━━━╮        \ 033 [1; 92m DARK \ 033 [1; 93m" )
+cetak ( "╰┳╯          \ 033 [1; 91m ◢▉◣       \ 033 [1; 94m FB \ 033 [1; 93m" )
+cetak ( " \ 033 [1; 93m ┃      \ 033 [1; 91m ▉▉▉" )
+cetak ( " \ 033 [1; 93m ┃            \ 033 [1; 91m ◥▉◤" )
+cetak ( " \ 033 [1; 93m ┃ ╭━┳━━━━╯" )
+cetak ( "┣━━━━━━┫         \ 033 [1; 91mAuthor \ 033 [1; 97m: \ 033 [1; 92mSabil \ 033 [1; 93m" )
+mencetak ( "╭╯ ╰╮        \ 033 [1; 91mYotube \ 033 [1; 97m: \ 033 [1; 92mHackSael" )
+mencetak ( " \ 033 [1; 91m [ \ 033 [1; 96m ================================== ======= \ 033 [1; 91m] " )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m1 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkXploit" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m2 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkfbvip" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m3 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkFb v.7" ))
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m4 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkFbking" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m5 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkFbOld" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m6 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkFbv.9" " )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m7 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkFbStoriku" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m8 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkFbRusmana" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m9 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mDarkAutoMbf" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m10 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; Tutorial 92m Tidak Cp" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m11 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; HackFbv2.3" )
+cetak ( " \ 033 [1; 92m [ \ 033 [1; 97m00 \ 033 [1; 92m] \ 033 [1; 97m. \ 033 [1; 92mExit" )
+pil  =  str ( input ( " \ 033 [1; 96m [Pilih Nomor \ 033 [1; 97m: \ 033 [1; 92m $" ))
+jika  pil  ==  "1" :
+   cetak ( " \ 033 [1; 92memasang ..." )
+   tidur ( 2 )
+   os . sistem ( "git clone https://github.com/BangDanz/darkxploit" )
+   os . sistem ( "cd darkxploit" )
+   os . sistem ( "python2 darkfb.py" )
+elif  pil  ==  "2" :
+     cetak ( " \ 033 [1; 92mInstalasi ...." )
+     os . sistem ( "git clone https://github.com/TheMagizz/DarkVip" )
+     os . sistem ( "cd DarkVip" )
+     os . sistem ( "python2 darkvip.py" )
+elif  pil  ==  "3" :
+     cetak ( " \ 033 [1; 92mInstalasi ...." )
+     os . sistem ( "git clone https://github.com/perijoko14/dark" )
+     os . sistem ( "cd dark" )
+     os . sistem ( "python2 dark.py" )
+elif  pil  ==  "4" :
+     cetak ( " \ 033 [1; 92memasang ..." )
+     os . sistem ( "git clone https://github.com/saydog/DARK-KING" )
+     os . sistem ( "cd DARK-KING" )
+     os . sistem ( "python2 dog.py" )
+elif  pil  ==  "5" :
+     cetak ( " \ 033 [1; 92mInstalasi" )
+     os . sistem ( "git clone https://github.com/pashayogi/SETAN" )
+     os . sytem ( "cd SETAN" )
+     os . sistem ( "python2 SETAN.py" )
+elif  pil  ==  "6" :
+     cetak ( " \ 033 [1; 92mInstalasi ...." )
+     os . sistem ( "git clone https://gitlab.com/Sumarr-ID/darkfbv9" )
+     os . sistem ( "cd darkfbv9" )
+     os . sistem ( "python2 dark.py" )
+elif  pil  ==  "7" :
+     cetak ( " \ 033 [1; 92mInstalasi ...." )
+     os . sistem ( "git clone https://github.com/storiku/darkfb" )
+     os . sistem ( "cd darkfb" )
+     os . sistem ( "python2 Dark.py" )
+elif  pil  ==  "8" :
+     cetak ( " \ 033 [1 °; 92mMenginstal ...." )
+     os . sistem ( "git clone https://github.com/Rusmana-ID/rus" )
+     os . sistem ( "cd rus" )
+     os . sistem ( "cd Tools" )
+     os . sistem ( "cd fb3" )
+     os . sistem ( "python2 hack-fb.py" )
+Elif  pil  ==  "9" :
+     mencetak ( " \ 033 [1; 92mInstalasi ...." )
+     os . sistem ( "git clone https://github.com/BangDanz/autombf" )
+     os . sistem ( "cd autombf" )
+     os . sistem ( "python2 hack.py" )
+Elif  pil  ==  "10" :
+     mencetak ( " \ 033 [1; 92mMemuat ....." )
+     waktu . tidur ( 2.3 )
+     os . sistem ( "hapus" )
+     tidur ( 2 )
+     mencetak ( "CARA SUAPA AKUN TIDAK KENA CP" )
+     tidur ( 2 )
+     mencetak ( "1.Lu harus buat akun fb baru" )
+     tidur ( 2 )
+     mencetak ( "2.ttl harus 1988" )
+     tidur ( 2 )
+     mencetak ( "3.menggunakan termux zsh" )
+     tidur ( 2 )
+     mencetak ( "4.login menggunakan desktop opera mini" )
+     waktu .lengan ( 2 )
+     mencetak ( "selesai ..." )
+     waktu . tidur ( 2 )
+     os . sistem ( "python multidarkfb.py" )
+Elif pil =="11" :
+     mencetak ( " \ 033 [1; 92mInstalasi ...." )
+     os . sistem ( "git clone https://github.com/ToolBaru/HackFbv2.3" )
+     os . sistem ( "cd HackFbv2.3" )
+     os . sistem ( "python2 dkfb" )
+elif  pil  ==  "00" :
+     cetak ( " \ 033 [1; 92mTERIMAKASIH SUDAH PAKAI TOOLS INI" )
+     tidur ( 2 )
+     mencetak ( "Bye Tod * _" )
+lain :
+  cetak ( " \ 033 [1; 92mInput tidak ada" )
+  tidur (2 )
+  os . sistem ( "python2 multidarkfb.py" )
